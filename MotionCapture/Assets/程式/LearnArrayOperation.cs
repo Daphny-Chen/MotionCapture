1a816ce5da93ac45e98b37978ac8612a43a79238
Daphny-Chen <55218517+Daphny-Chen@users.noreply.github.com>
2019-10-17 10:24:37 +0800

    1017

---
+++ MotionCapture/Assets/程式/LearnArrayOperation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnArrayOperation : MonoBehaviour {
+    public int[] students = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(students[6]);
+        students[2] = 33;
+        Debug.Log("學生陣列長度" + students.Length);
+    }
+}
---
+++ MotionCapture/Assets/程式/LearnCSharp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnCSharp : MonoBehaviour {
+    [Header("分數")]
+    [Range(0, 100)]
+    public int score = 60;
+    [Header("速度"), Tooltip("可以放紅色要藍色藥水")]
+    public float speed = 1.5f;
+    [Header("道具")]
+    public string prop = "藥水";
+    [Header("任務")]
+    public bool mission = false;
+
+    public Vector2 v2 = new Vector2(7, 77);
+    public Vector3 v3 = new Vector3(1, 2, 3);
+
+    public Color blue = Color.blue;
+    public Color rad = new Color(0.7f, 0.1f, 0.2f);
+
+    public AudioClip Sound;
+    public Camera cam;
+    public Light lig;
+    public Transform camPos;
+    public GameObject obj;
+
+    public Debug deb;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        //非靜態類別
+        //Camera.depth=10.5f;//
+        cam.depth = 10.5f;
+
+        //靜態類別
+        Debug.Log("123");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
